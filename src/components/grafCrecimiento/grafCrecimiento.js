a46cb4f315878a1a4c99b4236a4f62a4bbbc8bc8
GarsePersen <sebastian.garay.p@usach.cl>
2018-08-05 16:51:49 -0400

    Grafico crecimiento

--- src/components/grafCrecimiento/grafCrecimiento.js
+++ src/components/grafCrecimiento/grafCrecimiento.js
@@ -89,7 +89,7 @@ export default{
   },
   mounted:function(){
     let self = this;
-    fetch('http://165.227.12.119:9091/statistics/best10/genre/rock')
+    fetch('http://165.227.12.119:9091/statistics/best10/artistIncrease')
     .then(function(response) {
       return response.json();
     })
